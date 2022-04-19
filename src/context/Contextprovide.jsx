22ab17b79b2e0bf67895330f3fdab8d4dec7670b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-19 17:46:03 -0300

    feat: fixing bug

--- src/context/Contextprovide.jsx
+++ src/context/Contextprovide.jsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import context from './context';
 
 export default function Contextprovide({ children }) {
-  const [dataApi, setDataApi] = useState([]);
+  const [dataApi, setDataApi] = useState('');
+  console.log(dataApi, 'Estou no "Contextprovide"');
 
   return (
     <context.Provider value={ { dataApi, setDataApi } }>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useContext, useEffect } from 'react';
+import context from '../context/context';
+
 import Header from '../components/Header';
 
 export default function Home() {
+  const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  const { dataApi, setDataApi } = useContext(context);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setDataApi(data.meals[0]);
+      return data;
+    };
+    console.log(fetchData, 'Estou no "Home"');
+    // fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    //   .then((response) => {
+    //     // console.log(response);
+    //     return response.json()
+    //   })
+    //   .then((data) => {
+    //     console.log(data, 'Data completo');
+    //     console.log(data.meals[0], 'Data apenas com os dados');
+    //     setDataApi(data)
+    //   });
+  }, []);
+
   return (
     <div>
       <Header />
