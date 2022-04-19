16c6cd9655e1c05bf775ff4f765db1a74fccbcf7
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-04-19 17:48:12 -0300

    Login controller

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,12 +1,10 @@
 const { login } = require('../services/login');
 
-const findUser = async (req, res, next) => {
+module.exports  = async (req, res, next) => {
   try {
-    const data = await login(req.body);
-    res.status(200).json(data);
+    const { token } = await login(req.body);
+    res.status(200).json({ token });
   } catch (e) {
     next(e);
   }
 };
-
-module.exports = { findUser };
