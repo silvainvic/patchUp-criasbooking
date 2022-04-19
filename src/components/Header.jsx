081cd5fe5a02afdc0f9acf54b225fbc6314ff123
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-19 15:51:32 -0300

    feat: created "src/components"

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <h1>Header 01</h1>
+      </div>
+      <div>
+        <h1>Header 02</h1>
+      </div>
+      <div>
+        <h1>Header 03</h1>
+      </div>
+    </div>
+  );
+}
