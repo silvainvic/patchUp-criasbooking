49f030c145906413b80c44e6250dfd40db6d8580
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-19 15:45:42 -0300

    feat: configured routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import UserRegister from './pages/UserRegister';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+        <Route path="/userRegister" element={ <UserRegister /> } />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
