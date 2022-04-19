96e83aeabaefe1d67e90a4dc4c99d28806afd06a
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-04-19 17:47:06 -0300

    Implemeting cors

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { parsed: { PORT } } = require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const route = require('./src/routes');
-const app = express()
+const app = express();
+
+app.use(cors());
+
+// const corsOptions = {
+//   origin: '',
+//   optionsSuccessStatus: 200,
+// };
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -10,4 +18,4 @@ app.use(express.json());
 
 app.use(route);
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
