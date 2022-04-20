6818ebcb3e2dad3a7ac2fe432c86a4021d892d7e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-20 16:47:01 -0300

    feat: created function "selectForm" in file "./front-end/src/components/Forms.jsx" line "04"

--- front-end/src/components/Forms.jsx
+++ front-end/src/components/Forms.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 
-export default function Forms() {
+export default function Forms({ history }) {
+  const selectForm = (pathname) => {
+    switch (pathname) {
+      case "/login":
+        return (
+          <div>
+            <form >
+              <label htmlFor="registerEmail">
+                Email:
+                <input id="registerEmail" type="email" placeholder="Digite seu email" />
+              </label>
+              <label htmlFor="registerPassword">
+                Password:
+                <input id="registerPassword" type="password" placeholder="Digite sua senha" />
+               </label>
+            </form>
+          </div>
+        );
+       default:
+        return (
+          <div>
+            <form >
+              <label htmlFor="registerName">
+                Name:
+                <input id="registerName" type="text" placeholder="Digite seu nome completo" />
+              </label>
+              <label htmlFor="registerEmail">
+                Email:
+                <input id="registerEmail" type="email" placeholder="Digite seu email" />
+              </label>
+              <label htmlFor="registerPassword">
+                Password:
+                <input id="registerPassword" type="password" placeholder="Digite sua senha" />
+               </label>
+            </form>
+          </div>
+        );
+    };
+  };
+
   return (
-    <div>
-      <h1>Forms</h1>
-    </div>
+    <form>
+      { selectForm(history) }
+    </form>
   );
 }
