37f7119482278119a80cd560fc8f99059287f9e5
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-23 17:28:33 -0300

    fix: fixed destructring function "service" in line "5"

--- back-end/src/controllers/register.js
+++ back-end/src/controllers/register.js
@@ -1,7 +1,8 @@
 const service = require('../services/register');
 
 module.exports = async ({ body: { name, email, password } }, res, next) => {
-  const { code, message, data } = await service({ name, email, password });
+  // const { code, message, data } = await service({ name, email, password });
+  const { code, message } = await service({ name, email, password });
 
   if (message) return res.status(code).json({ message });
 
