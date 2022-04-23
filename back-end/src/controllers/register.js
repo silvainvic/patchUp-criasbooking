e2004ee9a97a2fa2f9e7de58cf68b67a4551ecc7
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-23 18:58:26 -0300

    refactor: inserted new message "Usuário cadastrado com sucesso!"

--- back-end/src/controllers/register.js
+++ back-end/src/controllers/register.js
@@ -1,10 +1,12 @@
 const service = require('../services/register');
+const stt = require('../utils/status');
+const mess = require('../utils/message');
 
 module.exports = async ({ body: { name, email, password } }, res, next) => {
-  // const { code, message, data } = await service({ name, email, password });
-  const { code, message } = await service({ name, email, password });
+  const { message, data } = await service({ name, email, password });
 
-  if (message) return res.status(code).json({ message });
+  if (!message) return res.status(stt.STATUS_CREATED).json(mess.USER_REGISTER_OK);
 
-  next();
+  res.status(stt.STATUS_CONFLICT).json({ message });
+
 }
