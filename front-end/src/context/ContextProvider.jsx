73ad631ecce58460ab3089be3cdac8c0c8071dfb
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-24 19:56:07 -0300

    fix: change removed "useEffect"

--- front-end/src/context/ContextProvider.jsx
+++ front-end/src/context/ContextProvider.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import MyContext from './context';
+// import { getLocalStorage } from '../service/serviceLocalStorage';
 
 export default function ContextProvider({ children }) {
-  const [dataApi, setDataApi] = useState('');
-  console.log(dataApi, 'Estou no "ContextProvider"');
+  const [dataApi, setDataApi] = useState(''); 
 
   return (
     <MyContext.Provider value={ { dataApi, setDataApi } }>
