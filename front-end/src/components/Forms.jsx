34cf45150474c33b2b745a3557d0f3acb66d4bd4
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-04-26 18:43:24 -0300

    Styling register form

--- front-end/src/components/Forms.jsx
+++ front-end/src/components/Forms.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Forms({ history, getDataForm }) {
   const [dataForm, setDataForm] = useState({});
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = ({ target }) => {
     setDataForm({
@@ -16,16 +16,30 @@ export default function Forms({ history, getDataForm }) {
     switch (pathname) {
       case "/login":
         return (
-          <form className="flex flex-col space-y-2" onSubmit={ (event) => getDataForm(event, dataForm) }>
-            <label htmlFor="registerEmail">
-              Email:
-            </label>
-            <input id="registerEmail" name="email" type="email" onChange={ handleChange } placeholder="Digite seu email" />
-            <label htmlFor="registerPassword">
-              Password:
-            </label>
-            <input id="registerPassword" name="password" type="password" onChange={ handleChange } placeholder="Digite sua senha" />
-            <div id="id-button-form-login" className="flex justify-center space-x-12">
+          <form
+            className="flex flex-col space-y-2"
+            onSubmit={(event) => getDataForm(event, dataForm)}
+          >
+            <label htmlFor="registerEmail">Email:</label>
+            <input
+              id="registerEmail"
+              name="email"
+              type="email"
+              onChange={handleChange}
+              placeholder="Digite seu email"
+            />
+            <label htmlFor="registerPassword">Password:</label>
+            <input
+              id="registerPassword"
+              name="password"
+              type="password"
+              onChange={handleChange}
+              placeholder="Digite sua senha"
+            />
+            <div
+              id="id-button-form-login"
+              className="flex justify-center space-x-12"
+            >
               <input
                 class="
                 bg-green-900
@@ -37,7 +51,9 @@ export default function Forms({ history, getDataForm }) {
                 border-white-500
                 hover:border-transparent
                 rounded"
-                type="submit" value="Enter" />
+                type="submit"
+                value="Enter"
+              />
               <button
                 class="
                   bg-green-900
@@ -49,36 +65,66 @@ export default function Forms({ history, getDataForm }) {
                   border-white-500
                   hover:border-transparent
                   rounded"
-                  onClick={() => navigate("/userRegister")}>
-                    Cadastre-se
+                onClick={() => navigate("/userRegister")}
+              >
+                Cadastre-se
               </button>
             </div>
           </form>
         );
-       default:
+      default:
         return (
-          <form onSubmit={ (event) => getDataForm(event, dataForm) }>
-            <label htmlFor="registerName">
-              Name:
-              <input id="registerName" name="registerInputName" type="text" onChange={ handleChange } placeholder="Digite seu nome completo" />
-            </label>
-            <label htmlFor="registerEmail">
-              Email:
-              <input id="registerEmail" name="registerInputEmail" type="email" onChange={ handleChange } placeholder="Digite seu email" />
-            </label>
-            <label htmlFor="registerPassword">
-              Password:
-              <input id="registerPassword" name="registerInputPassword" type="password" onChange={ handleChange } placeholder="Digite sua senha" />
-             </label>
-              <input type="submit" value="Cadastrar" />
-          </form>
+          <div className="w-screen flex justify-center">
+            <div className="md:w-full w-1/2 p-10 shadow-2xl bg-gray-50">
+              <form
+                onSubmit={(event) => getDataForm(event, dataForm)}
+                // className="md:w-full w-1/2 p-10 shadow-2xl bg-gray-50"
+              >
+                <label htmlFor="registerName">
+                  Name:
+                  <input
+                    id="registerName"
+                    name="registerInputName"
+                    type="text"
+                    onChange={handleChange}
+                    className="w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5"
+                    placeholder="Digite seu nome completo"
+                  />
+                </label>
+                <label htmlFor="registerEmail">
+                  Email:
+                  <input
+                    id="registerEmail"
+                    name="registerInputEmail"
+                    type="email"
+                    onChange={handleChange}
+                    className="w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5"
+                    placeholder="Digite seu email"
+                  />
+                </label>
+                <label htmlFor="registerPassword">
+                  Password:
+                  <input
+                    id="registerPassword"
+                    name="registerInputPassword"
+                    type="password"
+                    onChange={handleChange}
+                    className="w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5"
+                    placeholder="Digite sua senha"
+                  />
+                </label>
+                <input
+                  type="submit"
+                  // className="bg-red-500 rounded text-white px-5 py-1 mt-3"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 mt-3 rounded"
+                  value="Cadastrar"
+                />
+              </form>
+            </div>
+          </div>
         );
-    };
+    }
   };
 
-  return (
-    <div>
-      { selectForm(history) }
-    </div>
-  );
+  return <div>{selectForm(history)}</div>;
 }
