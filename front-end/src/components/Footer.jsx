ea29b9be601bbd28a8dbcc16a73724e2216d9e8b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-26 12:25:13 -0300

    feat: created component "Footer.jsx"

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer
+      className="
+      bg-green-900
+      w-full
+      h-24
+    ">
+      <h1>Footer</h1>
+    </footer>
+  );
+}
