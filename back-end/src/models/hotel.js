c2097886211fba0f05afa1ef575ee19ade4fe30d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-26 21:54:07 -0300

    feat: created "hotel" model and migration

---
+++ back-end/src/models/hotel.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Hotel extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Hotel.init({
+    name: DataTypes.STRING,
+    categoryStars: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Hotel',
+  });
+  return Hotel;
+};
