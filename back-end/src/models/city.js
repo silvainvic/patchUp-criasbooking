9e4067ecbb4129c746f36066851007a36a6e977a
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-28 12:37:19 -0300

    fix: created base file structure "src/models/city"

--- back-end/src/models/city.js
+++ back-end/src/models/city.js
@@ -1,23 +1,13 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  City.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'City',
+  const City = sequelize.define("City", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
-  return City;
+
+  City.associate = (models) => {
+    City.hasMany(models.Hotel, { foreignKey: 'cityId', as: 'hotels' });
+    City.belongsTo(models.State, { foreignKey: 'stateId', as: 'state',});
+  };
 };
