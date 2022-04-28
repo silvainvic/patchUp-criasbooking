280f417a7f1e9fca9399884e6cec0fa5e8d8b725
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-28 12:32:25 -0300

    feat: created base file structure "sc/models/state"

--- back-end/src/models/state.js
+++ back-end/src/models/state.js
@@ -1,23 +1,12 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class State extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  State.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'State',
-  });
-  return State;
+  const State = sequelize.define("State", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, { timestamps: false });
+
+  State.associate = (models) => {
+    State.hasMany(models.City, { foreignKey: 'stateId', as: 'cities' });
+  };
 };
