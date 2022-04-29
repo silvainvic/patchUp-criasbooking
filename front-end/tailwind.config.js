3e910005dbfd3f3ee9c03e1dc14aa58443038a57
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-29 19:19:13 -0300

    feat: installed plugin "daisyUI"

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
         'criasBooks': "url('src/img/criasbookingimg.png')",
       },
       colors: {
-        theme: '#8bc640'
+        theme: '#8bc640',
+        colorCriasBooking: '#8AC53F',
       }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
   screens: {
     '2xl': {'max': '1535px'},
     // => @media (max-width: 1535px) { ... }
