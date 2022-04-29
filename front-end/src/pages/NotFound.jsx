d7dde364446e0c89c8bc9550ba163b1c35bebc07
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-29 10:43:16 -0300

    feat: add "tailwind" in file "front-end/src/pages/NotFound.jsx"

--- front-end/src/pages/NotFound.jsx
+++ front-end/src/pages/NotFound.jsx
@@ -5,13 +5,32 @@ export default function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <div id="main-div-notFound">
-      <div id="message-div-notFound">
+    <div id="main-div-notFound"
+      className="
+        w-full h-screen
+        flex flex-col items-center justify-center
+        bg-green-400
+        text-red-700 text-4xl
+        space-y-8"
+    >
+      <div id="message-div-notFound"
+        className="flex flex-col items-center justify-center"
+      >
           <h1>Ops! "Algo de errado não está certo"</h1>
           <h2>Página não encontrada</h2>
       </div>
-      <div id="button-return-notFound">
-        <button onClick={ () => navigate('/') }>Voltar para Home</button>
+      <div id="button-return-notFound"
+        class="
+        bg-green-900
+        hover:bg-white
+        text-white hover:text-green-500
+        font-semibold
+        py-2 px-4 border
+        border-white-500 hover:border-black
+        hover:border-transparent
+        rounded"
+      >
+        <button onClick={ () => navigate('/') }>Voltar</button>
       </div>
     </div>
   );
