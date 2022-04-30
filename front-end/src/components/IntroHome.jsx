c78a1f32f3b41b08d38730b3bd88e85e4f1ac06f
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-30 11:42:56 -0300

    feat: created file "src/components/IntroHome.jsx"

---
+++ front-end/src/components/IntroHome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function IntroHome() {
+  return (
+      <div>
+        <h1 className="">Pesquise hotéis e pousadas por destino.</h1>
+        <h3>Escolha um destino e o CriasBooking ajuda você a encontrar o melhor hotel com menor preço!</h3>
+      </div>
+  );
+}
