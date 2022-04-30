5c0277a930134e38a02fa8c0590face285ff83aa
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-30 13:07:08 -0300

    feat: created file "front-end/src/help/arrStateCity.js

---
+++ front-end/src/help/arrStateCity.js
@@ -0,0 +1,112 @@
+export default function arrStateAndCity() {
+  return [
+    {
+      state: 'Acre',
+      city: 'Rio Branco',
+    },
+    {
+      state: 'Alagoas',
+      city: 'Maceió',
+    },
+    {
+      state: 'Amapá',
+      city: 'Macapá',
+    },
+    {
+      state: 'Amazonas',
+      city: 'Manaus',
+    },
+    {
+      state: 'Bahia',
+      city: 'Salvador',
+    },
+    {
+      state: 'Ceará',
+      city: 'Fortaleza',
+    },
+    {
+      state: 'Distrito Federal',
+      city: 'Brasília',
+    },
+    {
+      state: 'Espírito Santo',
+      city: 'Vitória',
+    },
+    {
+      state: 'Goiás',
+      city: 'Goiânia',
+    },
+    {
+      state: 'Maranhão',
+      city: 'São Luís',
+    },
+    {
+      state: 'Mato Grosso',
+      city: 'Cuiabá',
+    },
+    {
+      state: 'Mato Grosso do Sul',
+      city: 'Campo Grande',
+    },
+    {
+      state: 'Minas Gerais',
+      city: 'Belo Horizonte',
+    },
+    {
+      state: 'Pará',
+      city: 'Belém',
+    },
+    {
+      state: 'Paraíba',
+      city: 'João Pessoa',
+    },
+    {
+      state: 'Paraná',
+      city: 'Curitiba',
+    },
+    {
+      state: 'Pernambuco',
+      city: 'Recife',
+    },
+    {
+      state: 'Piauí',
+      city: 'Teresina',
+    },
+    {
+      state: 'Rio de Janeiro',
+      city: 'Rio de Janeiro',
+    },
+    {
+      state: 'Rio Grande do Norte',
+      city: 'Natal',
+    },
+    {
+      state: 'Rio Grande do Sul',
+      city: 'Porto Alegre',
+    },
+    {
+      state: 'Rondônia',
+      city: 'Porto Velho',
+    },
+    {
+      state: 'Roraima',
+      city: 'Boa Vista',
+    },
+    {
+      state: 'Santa Catarina',
+      city: 'Florianópolis',
+    },
+    {
+      state: 'São Paulo',
+      city: 'São Paulo',
+    },
+    {
+      state: 'Sergipe',
+      city: 'Aracaju',
+    },
+    {
+      state: 'Tocantins',
+      city: 'Palmas',
+    },
+  ];
+} 
