75df476efe84373b4f87ae321204cee755a3aca5
Renata <renatamagno@live.com>
2022-04-30 20:55:49 -0300

    feat: add route register

---
+++ back-end/src/routes/register.js
@@ -0,0 +1,8 @@
+const register = require('../controllers/register');
+const { isValidInputs, isValidRegister } = require('../middlewares/schema');
+
+const registerRoute = require('express').Router();
+
+registerRoute.post('/', isValidRegister, register);
+
+module.exports = registerRoute;
