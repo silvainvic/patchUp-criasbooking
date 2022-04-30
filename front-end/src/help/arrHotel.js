758cc443d8ba0e380adae2b703554d276a9cfa9e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-04-30 13:07:44 -0300

    feat: created file "front-end/src/help/arrHotel.js"

---
+++ front-end/src/help/arrHotel.js
@@ -0,0 +1,16 @@
+export default function arrHotel() {
+    return [
+      {
+        hotel: 'Hotel 1',
+      },
+      {
+        hotel: 'Hotel 2',
+      },
+      {
+        hotel: 'Hotel 3',
+      },
+      {
+        hotel: 'Hotel 4',
+      },
+    ];
+  } 
