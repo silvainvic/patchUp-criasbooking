80a3fb0c1385c193225223ed23d0bf38496e1566
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-04-30 17:39:27 -0300

    Improving footer

--- front-end/src/components/MemberGroup.jsx
+++ front-end/src/components/MemberGroup.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { MdWeb } from 'react-icons/md';
 
 export default function MemberGroup(props) {
   return (
     <div class="w-full px-4 lg:w-1/3 md:w-1/2">
+      {/* <img src={require(`./${props.image}`)} alt="" /> */}
       <h2 class="mb-2 font-bold tracking-widest text-gray-900">{props.name}</h2>
       <ul class="mb-8 space-y-2 text-sm list-none">
         <li className="flex justify-center items-center">
           <FaLinkedin />
-          <a href={props.linkedin} class="text-white hover:text-gray-800 ml-1">
+          <a
+            href={props.linkedin}
+            class="text-white hover:text-gray-800 ml-1"
+            target="_blank"
+            rel="noreferrer"
+          >
             Linkedin
           </a>
         </li>
         <li className="flex justify-center items-center">
           <FaGithub />
-          <a href={props.github} class="text-white hover:text-gray-800 ml-1">
+          <a
+            href={props.github}
+            class="text-white hover:text-gray-800 ml-1"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub
           </a>
         </li>
         <li className="flex justify-center items-center">
-          <FaMailBulk />
-          <a href={props.email} class="text-white hover:text-gray-800 ml-1">
-            E-mail
-          </a>
-        </li>
-        <li className="flex justify-center items-center">
           <MdWeb />
-          <a href={props.portfolio} class="text-white hover:text-gray-800 ml-1">
+          <a
+            href={props.portfolio}
+            class="text-white hover:text-gray-800 ml-1"
+            target="_blank"
+            rel="noreferrer"
+          >
             Portfólio
           </a>
         </li>
---
+++ front-end/src/data/members.js
@@ -0,0 +1,32 @@
+const members = [
+  {
+    name: 'Ary Barbosa',
+    image: 'ary.png',
+    linkedin: 'https://www.linkedin.com/in/aryherton-dos-santos-barbosa-202441b8/',
+    github: 'https://github.com/aryherton',
+    portfolio: '',
+  },
+  {
+    name: 'Grazziano Fagundes',
+    image: 'grazziano.png',
+    linkedin: 'https://www.linkedin.com/in/grazziano-fagundes/',
+    github: 'https://github.com/Grazziano',
+    portfolio: '',
+  },
+  {
+    name: 'Renata Magno',
+    image: 'renata.png',
+    linkedin: 'https://www.linkedin.com/in/renatamagno/',
+    github: 'https://github.com/re-magno',
+    portfolio: '',
+  },
+  {
+    name: 'Sávio Silva',
+    image: 'savio.png',
+    linkedin: 'https://www.linkedin.com/in/silvainvic/',
+    github: 'https://github.com/silvainvic',
+    portfolio: '',
+  },
+];
+
+module.exports = { members };
