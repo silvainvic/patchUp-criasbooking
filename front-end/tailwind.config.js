2ebf4558845dc135b3dfea88492195d98a23f27d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-01 17:28:48 -0300

    feat: inserted color palette in file "tailwind.config.js"

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       colors: {
         theme: '#8bc640',
         colorCriasBooking: '#8AC53F',
+        colorP01Green: '#8AC53F',
+        colorP02Green: '#DAEBC4',
+        colorP03Green: '#E4F2CE',
+        colorP04Beige: '#E4F2CE',
+        colorP05Dark: '#0D0D0D',
       }
     },
   },
