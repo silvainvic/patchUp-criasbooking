b60bac6712af6d166d5d3c7e1b80718507177b5e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-01 11:19:14 -0300

    fix: change text and inserted "tailwind"

--- front-end/src/components/IntroHome.jsx
+++ front-end/src/components/IntroHome.jsx
@@ -2,9 +2,36 @@ import React from "react";
 
 export default function IntroHome() {
   return (
-      <div>
-        <h1 className="">Pesquise ofertas de hotéis e pousadas</h1>
-        <h3>O CriasBooking ajuda você a encontrar hotéis baratos, aluguéis de férias, pousadas, resorts e muito mais. Planeje sua viagem de forma rápida e fácil.</h3>
+      <div className="flex flex-col items-center">
+        <p>
+          <h1
+            className="
+              pt-2
+              text-4xl
+              font-bold
+              text-yellow-600"
+          >
+            Pesquise ofertas de hotéis e pousadas
+          </h1>
+        </p>
+        <p>
+          <h3
+            className="
+              text-xl
+              text-gray-600"
+          >
+            O CriasBooking ajuda você a encontrar hotéis baratos, aluguéis de férias, pousadas, resorts e muito mais.
+          </h3>
+        </p>
+        <p>
+          <h3
+            className="
+              text-xl
+              text-gray-600"
+          >
+            Planeje sua viagem de forma rápida e fácil.
+          </h3>
+        </p>
       </div>
   );
 }
