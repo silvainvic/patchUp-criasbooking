d739446783b1784c13d10dcffd16e11e1038ba06
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-01 11:04:23 -0300

    fix: change texto tag "h3" in file "front-end/src/components/IntroHome.jsx" line 7

--- front-end/src/components/IntroHome.jsx
+++ front-end/src/components/IntroHome.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function IntroHome() {
   return (
       <div>
-        <h1 className="">Pesquise hotéis e pousadas por destino.</h1>
-        <h3>Escolha um destino e o CriasBooking ajuda você a encontrar o melhor hotel com menor preço!</h3>
+        <h1 className="">Pesquise ofertas de hotéis e pousadas</h1>
+        <h3>O CriasBooking ajuda você a encontrar hotéis baratos, aluguéis de férias, pousadas, resorts e muito mais. Planeje sua viagem de forma rápida e fácil.</h3>
       </div>
   );
 }
