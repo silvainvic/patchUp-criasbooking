a98b098f81b22ef5fd6d5bdbca65c7547809a3cb
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-01 11:20:28 -0300

    fix: change "tailwind" line 21, 24

--- front-end/src/components/SelectReservation.jsx
+++ front-end/src/components/SelectReservation.jsx
@@ -18,11 +18,15 @@ export default function SelectReservation() {
     <div
       className="
         flex justify-center
-        w-full h-12
-        bg-ccolorCriasBooking"
+        w-full h-20
+        bg-colorCriasBooking"
     >
-      <form className="w-full">
-        <select className="select select-success w-full max-w-xs" name="state" >
+      <form
+        className="
+          flex justify-center items-center space-x-6
+          w-full"
+      >
+        <select className="select select-success w-44 max-w-xs" name="state" >
           {arr && arr.map((item, index) => (
             <option
               key={ index }
@@ -33,22 +37,28 @@ export default function SelectReservation() {
             </option>
           ))}
         </select>
-        <select className="select select-success w-full max-w-xs" name="city" >
+        <select className="select select-success w-56 max-w-xs" name="city" >
           {stateCity
             ? ( <option value={ stateCity.city }>{ stateCity.city }</option> )
-            : ( <option>Selecione uma estado</option> )}
+            : ( <option>Selecione um estado</option> )}
         </select>
-        <select className="select select-success w-full max-w-xs" name="hotel" >
+        <select className="select select-success w-44 max-w-xs" name="hotel" >
           {arrH && arrH.map((item, index) => (
             <option key={ index } value={ item.hotel }>{ item.hotel }</option>
           ))}
         </select>
-        <select name="dateStart">
-          <option value="dataStart">Data de início</option>
-        </select>
-        <select name="dateEnd">
-          <option value="dataEnd">Data final</option>
-        </select>
+        <input
+          type="date"
+          name="dateStart"
+          placeholder="Data de entrada"
+          class="input input-bordered input-success w-full max-w-xs"
+        />
+        <input
+          type="date"
+          name="dateEnd"
+          placeholder="Data de saída"
+          class="input input-bordered input-success w-full max-w-xs"
+        />
       </form>
     </div>
   );
