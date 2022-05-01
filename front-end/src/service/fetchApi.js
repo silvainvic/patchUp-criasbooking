8ab46c6facd6d4c665c7459bafe4138d81ef9bae
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-01 20:31:42 -0300

    feat: inserted function "fetHotels" in file "front-end/src/service/fetchApi.js"

--- front-end/src/service/fetchApi.js
+++ front-end/src/service/fetchApi.js
@@ -1,5 +1,6 @@
 const URL_CRIAS_REGISTER = 'http://localhost:3001/register';
 const URL_CRIAS_LOGIN = 'http://localhost:3001/login';
+const URL_HOTEL = 'http://localhost:3001/hotels';
 
 export async function fetchRegister(name, email, password) {
   return fetch(URL_CRIAS_REGISTER,
@@ -45,4 +46,21 @@ export async function fetchLogin(email, password) {
             .then((token) => {
               return token;
             });
+}
+
+export async function fetchHotels() {
+  return fetch(URL_HOTEL, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((hotels) => {
+      console.log('kkkkkkkkkkk', hotels);
+      return hotels;
+    });
 }
