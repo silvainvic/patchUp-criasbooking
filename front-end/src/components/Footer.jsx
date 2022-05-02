f00c18926f5203d0b6ddd3dc2fb03bdbb958c37d
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-02 19:48:37 -0300

    feat: auto render inserted "map"

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import { members } from '../data/members';
+import { members } from '../data/members';
 import MemberGroup from './MemberGroup';
-import ary from '../img/members/ary.png';
-import grazziano from '../img/members/grazziano.jpg';
-import renata from '../img/members/renata.jpg';
-import savio from '../img/members/savio.jpg';
+// import ary from '../img/members/ary.png';
+// import grazziano from '../img/members/grazziano.jpg';
+// import renata from '../img/members/renata.jpg';
+// import savio from '../img/members/savio.jpg';
 
 export default function Footer() {
   return (
@@ -14,7 +14,7 @@ export default function Footer() {
           container
           flex flex-col flex-wrap
           px-4
-          py-16
+          py-12
           mx-auto
           md:items-center
           lg:items-start
@@ -22,64 +22,18 @@ export default function Footer() {
         "
       >
         <div class="justify-between w-full mt-4 text-center lg:flex">
-          <>
-            <img
-              src={ary}
-              className="object-contain h-20 w-20 rounded-full"
-              alt=""
-            />
-            <MemberGroup
-              name="Ary Barbosa"
-              linkedin="https://www.linkedin.com/in/aryherton-dos-santos-barbosa-202441b8/"
-              github="https://github.com/aryherton"
-            />
-          </>
-          <>
-            <img
-              src={grazziano}
-              className="object-contain h-20 w-20 rounded-full"
-              alt=""
-            />
-            <MemberGroup
-              name="Grazziano Fagundes"
-              linkedin="https://www.linkedin.com/in/grazziano-fagundes/"
-              github="https://github.com/Grazziano"
-            />
-          </>
-          <>
-            <img
-              src={renata}
-              className="object-contain h-20 w-20 rounded-full"
-              alt=""
-            />
-            <MemberGroup
-              name="Renata Magno"
-              linkedin="https://www.linkedin.com/in/renatamagno/"
-              github="https://github.com/re-magno"
-            />
-          </>
-          <>
-            <img
-              src={savio}
-              className="object-contain h-20 w-20 rounded-full"
-              alt=""
-            />
-            <MemberGroup
-              name="Sávio Silva"
-              linkedin="https://www.linkedin.com/in/silvainvic/"
-              github="https://github.com/silvainvic"
-            />
-          </>
-          {/* {members.map((ele) => {
+          {members.map((ele, index) => {
             return (
               <MemberGroup
+                keyMem={index}
+                image={ele.image}
                 name={ele.name}
                 linkedin={ele.linkedin}
                 github={ele.github}
                 portfolio={ele.portfolio}
               />
             );
-          })} */}
+          })}
         </div>
       </div>
       <div class="flex justify-center -mt-12">
