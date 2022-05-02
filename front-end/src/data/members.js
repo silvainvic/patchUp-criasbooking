24ebc50fbbaf498bfc2ede37668344580df035a2
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-02 19:51:20 -0300

    fix: change key "image" in file "front-end/src/data/members.js"

--- front-end/src/data/members.js
+++ front-end/src/data/members.js
@@ -1,28 +1,28 @@
 const members = [
   {
     name: 'Ary Barbosa',
-    image: '../img/members/ary.png',
+    image: 'https://i.imgur.com/aLmzu9p.png',
     linkedin: 'https://www.linkedin.com/in/aryherton-dos-santos-barbosa-202441b8/',
     github: 'https://github.com/aryherton',
     portfolio: '',
   },
   {
     name: 'Grazziano Fagundes',
-    image: '../img/members/grazziano.png',
+    image: 'https://i.imgur.com/yv2wRLy.jpg',
     linkedin: 'https://www.linkedin.com/in/grazziano-fagundes/',
     github: 'https://github.com/Grazziano',
     portfolio: '',
   },
   {
     name: 'Renata Magno',
-    image: '../img/members/renata.png',
+    image: 'https://i.imgur.com/fAGd2lH.jpg',
     linkedin: 'https://www.linkedin.com/in/renatamagno/',
     github: 'https://github.com/re-magno',
     portfolio: '',
   },
   {
     name: 'Sávio Silva',
-    image: '../img/members/savio.png',
+    image: 'https://i.imgur.com/KxQEEPk.jpg',
     linkedin: 'https://www.linkedin.com/in/silvainvic/',
     github: 'https://github.com/silvainvic',
     portfolio: '',
