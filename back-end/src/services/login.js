79f4bc45577fa88c7084d2286e192a1bc185713b
Savio Silva <s.drksilva@gmail.com>
2022-05-03 07:53:37 -0300

    refactor(services): change structure from code

--- back-end/src/services/login.js
+++ back-end/src/services/login.js
@@ -1,20 +1,16 @@
 
 const { User } = require('../models');
-const { createToken } = require('./manageToken');
 
-const login = async ({ email, password }, ip) => {
+const tokens = require('../middlewares/tokens');
+
+module.exports.login = async ({ email, password, ip }) => {
   const user = await User.findOne({ where: { email } });
 
-  if (!user || user.password !== password) {
-    throw ({ code: 400, message: 'Invalid fields'});
-  }
-  if (!ip) {
-    throw ({ code: 400, message: 'Not ip'});
-  }
-  
-  const token = createToken(email, password, ip);
+  if (!user || user.password !== password) return { code: 400, message: 'Invalid fields' };
 
-  return { token };
-};
+  if (!ip) return { code: 400, message: 'Invalid ip' };
 
-module.exports = { login };
+  const token = tokens.generate({ ip, name: user.name, email, password });
+
+  return { code: 200, token };
+}
