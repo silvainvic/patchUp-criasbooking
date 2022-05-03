ffc7784e05c600a2b080cc5ac36b6c4947be46f8
Savio Silva <s.drksilva@gmail.com>
2022-05-03 10:42:30 -0300

    feat(routes): add routes to hotels
    add routes to 'get all', 'get by id' and 'create' new hotels

--- back-end/src/routes/hotels.js
+++ back-end/src/routes/hotels.js
@@ -1,10 +1,31 @@
-// const express = require('express');
-// const hotels = require('../controllers/hotels');
-// const hotelsRoute = express.Router();
+const route = require('express').Router();
+const { celebrate, Segments, Joi } = require('celebrate');
 
-// hotelsRoute
-//   .get('/', hotels.getAll)
-//   .get('/:id', hotels.getById)
-//   .post('/', hotels.create);
+const controller = require('../controllers/hotels');
+const schemas = require('../middlewares/validations/schemas');
 
-// module.exports = hotelsRoute;
+route.route('/')
+  .get(controller.getAll)
+  .post(celebrate({
+    [Segments.HEADERS]: {
+      ...schemas.headers,
+      authorization: Joi.string().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().min(3).required(),
+      stars: Joi.number().required(),
+      state: Joi.string().min(3).required(),
+      city: Joi.string().min(3).required(),
+      district: Joi.string().min(3).required(),
+      address: Joi.string().min(3).required(),
+    }
+  }), controller.create);
+
+route.route('/:id')
+  .get(celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().positive().required(),
+    }
+  }), controller.getById);
+
+module.exports = route;
