ec32cc9ccd51674fb7a843b2e10bb348ad653553
Savio Silva <s.drksilva@gmail.com>
2022-05-03 08:29:56 -0300

    fix(services): add return in response from login

--- back-end/src/controllers/login.js
+++ back-end/src/controllers/login.js
@@ -1,11 +1,11 @@
-const { login } = require('../services/login');
+const service = require('../services/login');
 const ip = require('../services/getIp');
 
 module.exports.login = async (req, res, next) => {
   req.body.ip = ip.getIp(req);
 
-  const { code, message, token } = await login({ ...req.body });
-  if (token) res.status(code).json({ token });
+  const { code, message, token } = await service.login({ ...req.body });
+  if (token) return res.status(code).json({ token });
 
   next({ code, message });
 };
