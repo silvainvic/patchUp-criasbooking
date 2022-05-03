8d404224d4da1c0c1a4b48e2d499cebed9c69028
Savio Silva <s.drksilva@gmail.com>
2022-05-03 08:40:02 -0300

    delete(service): remove consumeApi

--- back-end/src/services/consumeAPI.js
+++
@@ -1,9 +0,0 @@
-const { useAxios } = require('../utils/api/fetchApi');
-
-module.exports = async (data) => {
-  const result = await useAxios(data);
-    
-  if (!result) return { code: 000, message: 'Failed requisition' }
-
-  return { data: result }
-};
