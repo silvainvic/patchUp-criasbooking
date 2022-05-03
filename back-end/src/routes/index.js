6546eef880bdd9b83082d1017eac5a55f0148c03
Savio Silva <s.drksilva@gmail.com>
2022-05-03 10:40:48 -0300

    feat(routes): add routes to hotels

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,8 +3,11 @@ const route = require('express').Router();
 const login = require('./login');
 const register = require('./register');
 
+const hotels = require('./hotels');
+
 route.use('/login', login);
 route.use('/register', register);
 
+route.use('/hotels', hotels);
 
 module.exports = route;
