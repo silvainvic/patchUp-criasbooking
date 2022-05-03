953761fdd2a82315bae96b47e3081298e65f94e8
Savio Silva <s.drksilva@gmail.com>
2022-05-03 07:57:39 -0300

    refactor(services): add capture of errors from login

--- back-end/src/services/login.js
+++ back-end/src/services/login.js
@@ -4,13 +4,19 @@ const { User } = require('../models');
 const tokens = require('../middlewares/tokens');
 
 module.exports.login = async ({ email, password, ip }) => {
-  const user = await User.findOne({ where: { email } });
+  try {
+    const user = await User.findOne({ where: { email } });
 
-  if (!user || user.password !== password) return { code: 400, message: 'Invalid fields' };
+    if (!user || user.password !== password) return { code: 400, message: 'Invalid fields' };
 
-  if (!ip) return { code: 400, message: 'Invalid ip' };
+    if (!ip) return { code: 400, message: 'Invalid ip' };
 
-  const token = tokens.generate({ ip, name: user.name, email, password });
+    const token = tokens.generate({ ip, name: user.name, email, password });
 
-  return { code: 200, token };
+    return { code: 200, token };
+  } catch (error) {
+    console.error(error.message);
+    return { code: 500, message: 'Erro interno do servidor' }
+  }
+ 
 }
