85c0bdac934d27c9ff92f5021779be4faf6bc711
Savio Silva <s.drksilva@gmail.com>
2022-05-04 11:12:05 -0300

    refactor(services): move login and register to an file

--- back-end/src/services/users.js
+++ back-end/src/services/users.js
@@ -1,7 +1,5 @@
-
 const { User, Group } = require('../models');
-
-const tokens = require('../middlewares/tokens');
+const tokens = require('../utils/tokens');
 
 module.exports.login = async ({ email, password, ip }) => {
   try {
@@ -12,15 +10,32 @@ module.exports.login = async ({ email, password, ip }) => {
     });
 
     if (!user || user.password !== password) return { code: 400, message: 'Invalid fields' };
-
     if (!ip) return { code: 400, message: 'Invalid ip' };
 
     const token = tokens.generate({ ip, id: user.id, name: user.name, email, level: user.group.level });
+    await user.update({ where: { id: user.id }, token });
 
     return { code: 200, token };
   } catch (error) {
     console.error(error.message);
     return { code: 500, message: 'Erro interno do servidor' }
   }
- 
+}
+
+module.exports.register = async ({ name, email, password, ip }) => {
+  try {
+    const already = await User.findOne({ where: { email } });
+
+    if (already) return { code: 409, message: 'User already exists' };
+  
+    const data = await User.create({ name, email, password, token: 'invalid' });
+    const token = tokens.generate({ ip, id: data.id, name, email, level: 0 });
+    
+    data.update({ token, where: { id: data.id } });
+
+    return { code: 201, token };
+  } catch (error) {
+    console.error(error.message);
+    return { code: 500, message: 'Erro interno do servidor' }
+  }
 }
