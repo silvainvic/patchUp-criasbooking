e3d91080b925d79acbad9ed271c213b392691a4e
Savio Silva <s.drksilva@gmail.com>
2022-05-08 11:41:48 -0300

    refactor(controller): change login
    add a new file to responsability from login

---
+++ back-end/src/controllers/login.js
@@ -0,0 +1,11 @@
+const service = require('../services/login');
+const ip = require('../utils/getIp');
+
+module.exports.login = async (req, res, next) => {
+  req.body.ip = ip.getIp(req);
+
+  const { code, message, token } = await service.login({ ...req.body });
+  if (token) return res.status(code).json({ token });
+
+  next({ code, message });
+};
