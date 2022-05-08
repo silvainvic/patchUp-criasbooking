8d4ddd518cdd455a6847d02784b6ed4be1c1c92b
Savio Silva <s.drksilva@gmail.com>
2022-05-08 13:42:13 -0300

    feat(service): add to user update update your data
    add funcionality to user update your name and email

---
+++ back-end/src/services/user.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+const tokens = require('../utils/tokens');
+
+module.exports.update = async ({ authorization, payload }) => {
+  try {
+    const { code, message, decoded } = tokens.decode(authorization);
+    if (!decoded) return { code, message };
+
+    const user = await User.findByPk(decoded.id);
+    const { id, name, email } = await user.update(payload);
+    
+    const token = tokens.generate({ ip: decoded.ip, id, name, email, level: decoded.level });
+    await user.update({ token });
+
+    return  { code: 200, data: 'updated success' }
+  } catch (error) {
+    console.error(error.message);
+    return { code: 500, message: 'Erro interno do servidor' };
+  }
+
+  
+}
+
+module.exports.updatePassword = async ({ authorization, payload }) => {}
